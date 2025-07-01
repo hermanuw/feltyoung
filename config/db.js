9d8b7f6ca9d1ef811571e15927ff50a79918bc6e
hermanu w <hermanu.widyatama11@gmail.com>
2025-07-01 21:07:08 +0700

    Adjust DB connection railway

--- config/db.js
+++ config/db.js
@@ -1,19 +1,13 @@
-const mysql = require('mysql2');
-require('dotenv').config(); // Load env variables
+const mysql = require("mysql2");
+require("dotenv").config(); // Load env variables
 
-const db = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  port     : process.env.DB_PORT,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error('❌ Koneksi ke database gagal:', err.message);
+    console.error("❌ Koneksi ke database gagal:", err.message);
   } else {
-    console.log('✅ Terhubung ke database MySQL');
+    console.log("✅ Terhubung ke database MySQL");
   }
 });
 
