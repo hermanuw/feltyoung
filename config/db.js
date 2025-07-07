4d5fd14615be3f723feb5df06e76ce1091adaa3d
hermanu w <hermanu.widyatama11@gmail.com>
2025-07-07 18:55:51 +0700

    Adjust DB Connection

--- config/db.js
+++ config/db.js
@@ -1,14 +1,6 @@
 const mysql = require("mysql2");
-require("dotenv").config(); // Load env variables
+require("dotenv").config();
 
-const db = mysql.createConnection(process.env.MYSQL_URL);
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ Koneksi ke database gagal:", err.message);
-  } else {
-    console.log("✅ Terhubung ke database MySQL");
-  }
-});
+const db = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = db;
