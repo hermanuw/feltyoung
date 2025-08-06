738cfa005ea31346ad3d9554f126c411f7562799
PramesRay <prames21001@mail.unpad.ac.id>
2025-08-07 03:26:23 +0700

    Fix Top Sales Component

--- src/views/dashboards/default/components/PopularStocks.vue
+++ src/views/dashboards/default/components/PopularStocks.vue
@@ -2,6 +2,7 @@
 import { ref, computed, watch, onMounted } from 'vue';
 import axios from '@/axios';
 import { ChevronUpIcon, ChevronDownIcon, ChevronRightIcon, DotsIcon } from 'vue-tabler-icons';
+import { max } from 'lodash';
 
 const topProducts = ref([]);
 const selectedPeriod = ref('today');
@@ -30,7 +31,7 @@ watch(selectedPeriod, fetchTopProducts);
     <v-card variant="outlined">
       <v-card-text>
         <div class="d-flex align-center">
-          <h4 class="text-h4 mt-1">Popular Stocks</h4>
+          <h4 class="text-h4 mt-1">Top 5 Sales</h4>
           <div class="ml-auto">
             <v-menu transition="slide-y-transition">
               <template v-slot:activator="{ props }">
@@ -58,18 +59,18 @@ watch(selectedPeriod, fetchTopProducts);
                 <h6 class="text-secondary text-h5">
                   {{ topProducts[0]?.name || '—' }}
                 </h6>
-                <span class="text-subtitle-2 text-medium-emphasis font-weight-bold"> {{ topProducts[0]?.total_sold || 0 }} Sold </span>
+                <span class="text-subtitle-2 text-medium-emphasis font-weight-bold"> {{ topProducts[0]?.brand }} </span>
               </div>
-              <h4 class="text-h4">{{ topProducts[0]?.total_sold || 0 }} Sold</h4>
+              <h4 class="text-h4"> {{ topProducts[0]?.total_sold || 0 }} Sold </h4>
             </div>
           </div>
         </v-card>
 
         <div class="mt-4">
-          <perfect-scrollbar :style="{ height: '270px' }">
+          <perfect-scrollbar :style="{ maxHeight: 'calc(60vh - 300px)' }">
             <v-list lines="two" class="py-0">
-              <v-list-item v-for="(item, i) in topProducts" :key="i" color="secondary" rounded="sm">
-                <template v-slot:append>
+              <v-list-item v-for="(item, i) in topProducts.slice(1)" :key="i" color="secondary" rounded="sm">
+                <!-- <template v-slot:append>
                   <div
                     :class="[
                       'rounded-sm d-flex align-center justify-center ml-3',
@@ -84,11 +85,11 @@ watch(selectedPeriod, fetchTopProducts);
                       :class="item.total_sold > 10 ? 'text-success' : 'text-error'"
                     />
                   </div>
-                </template>
+                </template> -->
                 <div class="d-inline-flex align-center justify-space-between w-100">
                   <div>
                     <h6 class="text-subtitle-1 font-weight-bold">{{ item.name }}</h6>
-                    <span :class="item.total_sold > 10 ? 'text-success' : 'text-error'"> {{ item.total_sold }} Sold </span>
+                    <span :class="item.total_sold > 10 ? 'text-success' : 'text-error'"> {{ item.brand }} </span>
                   </div>
                   <div class="ml-auto text-subtitle-1 text-medium-emphasis font-weight-bold">{{ item.total_sold }} Sold</div>
                 </div>
@@ -96,14 +97,14 @@ watch(selectedPeriod, fetchTopProducts);
             </v-list>
           </perfect-scrollbar>
 
-          <div class="text-center mt-3">
+          <!-- <div class="text-center mt-3">
             <v-btn color="primary" variant="text">
               View All
               <template v-slot:append>
                 <ChevronRightIcon stroke-width="1.5" width="20" />
               </template>
             </v-btn>
-          </div>
+          </div> -->
         </div>
       </v-card-text>
     </v-card>
